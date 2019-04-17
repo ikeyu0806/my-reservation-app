e7470857b35e95ad87f360571811c293c23dfaea
ikeyu0806 <ikeyu0806@gmail.com>
2019-04-17 11:49:23 +0900

    create form

--- app/controllers/reservation_controller.rb
+++ app/controllers/reservation_controller.rb
@@ -1,4 +1,10 @@
 class ReservationController < ApplicationController
-  def index
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.new(params)
+    @reservation.save
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: 'reservation#new'
+  resources :reservations
 end
