53a38d8b9a250f2327926f4189a13970f5f5a60f
ikeyu0806 <ikeyu0806@gmail.com>
2019-04-18 21:16:05 +0900

    add controller test

--- spec/controllers/reservation_controller_spec.rb
+++ spec/controllers/reservation_controller_spec.rb
@@ -3,21 +3,22 @@ require 'rails_helper'
 RSpec.describe ReservationController, type: :controller do
   let(:valid_attributes) do
     {
-      user_name: 'content',
-      mail: 'mail@examp;le.com',
+      user_name: 'ikegaya',
+      mail: 'test@example.com',
       phone_number: '09012345678',
-      date: '2019/4/01'
+      date: '2019-05-01 18:00:00'
     }
   end
 
   let(:invalid_attributes) do
     {
-      user_name: 'content',
-      mail: 'mail@examp;le.com',
-      phone_number: '09012345678',
-      date: '2019/4/01'
+      user_name: 'ikegaya',
+      mail: '',
+      phone_number: '',
+      date: ''
     }
   end
+
   describe 'GET #new' do
     it '正常に応答すること' do
       get :new, params: {}
@@ -27,22 +28,30 @@ RSpec.describe ReservationController, type: :controller do
 
   describe 'POST #create' do
     context '正常なパラメータの場合' do
+      it 'リクエストが成功すること' do
+        post :create, params: { reservation: valid_attributes }
+        expect(response.status).to eq 302
+      end
+
       it '正常に予約できること' do
         expect do
-          post :create, params: { post: valid_attributes }
+          post :create, params: { reservation: valid_attributes }
         end.to change(Reservation, :count).by(1)
       end
 
       it '予約ページにリダイレクトすること' do
-        post :create, params: { post: valid_attributes }
-        expect(response).to redirect_to(Reservation.last)
+        post :create, params: { reservation: valid_attributes }
+        expect(response).to redirect_to controller: :reservation, action: :new
       end
     end
 
     context '不正なパラメータの場合' do
-      it '正常に応答すること' do
-        post :create, params: { post: invalid_attributes }
-        expect(response).to be_successful
+      xit 'リクエストが成功すること' do
+        post :create, params: { reservation: invalid_attributes }
+        expect(response.status).to eq 200
+      end
+
+      xit '予約できないこと' do
       end
     end
   end
