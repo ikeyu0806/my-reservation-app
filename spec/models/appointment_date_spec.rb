310999be6b08d39e5fae3c1b9d209b2429a9dc18
ikeyu0806 <ikeyu0806@gmail.com>
2019-04-21 19:01:28 +0900

    test candidate_date

--- spec/models/appointment_date_spec.rb
+++ spec/models/appointment_date_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe AppointmentDate, type: :model do
   it { should validate_presence_of(:from_date) }
   it { should validate_presence_of(:to_date) }
   it { should belong_to(:event) }
+
+  describe 'candidate_dateメソッドの確認' do
+
+    subject { AppointmentDate.candidate_date(1) }
+    let!(:appointment_date) { create(:appointment_date) }
+    it '想定どおり候補日が返ること' do
+      expect(subject).to include 'Fri, 05 Apr 2019 15:00:00.000000000 +0000'
+    end
+  end
 end
