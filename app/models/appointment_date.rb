b8a84c3504cfffa839ba178f9141bd861fe9011c
ikeyu0806 <ikeyu0806@gmail.com>
2019-04-21 19:24:52 +0900

    excluded_date

--- app/models/appointment_date.rb
+++ app/models/appointment_date.rb
@@ -8,11 +8,13 @@ class AppointmentDate < ApplicationRecord
     appointment_date = AppointmentDate.find_by(event_id: event_id)
     from_date = appointment_date.from_date.to_date.strftime('%Y-%m-%d')
     to_date = appointment_date.to_date.to_date.strftime('%Y-%m-%d')
+    excluded_date = appointment_date.excluded_date.to_s
     wday = appointment_date.wday
     time = appointment_date.time
     candidate_date = []
     (DateTime.parse(from_date)..DateTime.parse(to_date)).each do |date|
-      if date.send(wday + "?")
+      date_string = date.strftime('%Y-%m-%d').to_s
+      if (date.send(wday + "?")) && (date_string != excluded_date)
         candidate_date << date + Rational(time.strftime('%H').to_i, 24)
       end
     end
